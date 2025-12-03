ad951dfbe121f5b3e24e55853e8c8d6e9994fdec
Faiver Bustos <faiver.bustos@gmail.com>
2025-12-02 19:39:01 -0500

    Se crea el esquema para el dashboard de paciente

--- src/app/pages/paciente-dashboard/paciente-dashboard.ts
+++ src/app/pages/paciente-dashboard/paciente-dashboard.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-paciente-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    CardModule,
+    TagModule,
+    DividerModule,
+    ButtonModule,
+    CheckboxModule
+  ],
   templateUrl: './paciente-dashboard.html',
   styleUrl: './paciente-dashboard.css',
 })
 export class PacienteDashboard {
+  medicamentos = [
+    { nombre: 'Metformina', dosis: '1 tableta' }
+  ];
 
+  alertaRiesgo = {
+    mensaje: 'Tu presión estuvo alta en las últimas 48 horas',
+    activo: true
+  };
+
+  proximaCita = '5 de marzo';
+
+  proximoControl = 'Glucosa';
+
+  tareasPendientes = [
+    { nombre: 'Glucosa' },
+    { nombre: 'Presión arterial' },
+    { nombre: 'Peso' }
+  ];
 }
